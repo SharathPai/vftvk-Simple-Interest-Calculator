6a2b14a00422bc67d1971a7a1e9403196e892dc3
Sharath B Pai <52992731+SharathPai@users.noreply.github.com>
2022-04-15 13:50:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}     
